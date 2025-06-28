4e6a4311f243af27717f203e20d25aa81edbd571
Ajay Shankar A <navaajayshankar333@gmail.com>
2025-06-28 12:58:17 +0530

    added required files for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ajay-shankar-portfolio-site/',
+  base: '/ajay-portfolio-site/',
   server: {
     host: "::",
     port: 8080,
